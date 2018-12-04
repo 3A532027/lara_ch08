db0645ec3940e1c7d0f57d39889d93f1f8712c86
3A532027 <s3a532027@student.nuct.edu.tw>
2018-12-04 23:37:16 +0800

    更正

---
+++ resources/views/board.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>詳細資料</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
+    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }} "></script>
+</head>
+    @extends('layouts.master')
+    @section('title','排行榜')
+    @section('content')
+        <div class="page-header">
+            <h1>排名</h1>
+        </div>
+        <div class="row">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>名次</th>
+                    <th>學號</th>
+                    <th>姓名</th>
+                    <th>國文</th>
+                    <th>英文</th>
+                    <th>數學</th>
+                    <th>總分</th>
+                    <th>動作</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($scores as $index => $score)
+                    <tr>
+                        <td>{{$index+1}}</td>
+                        <td>{{$score->student->no}}</td>
+                        <td>{{$score->student->user->name}}</td>
+                        <td>{{$score->chinese}}</td>
+                        <td>{{$score->english}}</td>
+                        <td>{{$score->math}}</td>
+                        <td>{{$score->total}}</td>
+                        <td>
+                            <a class="btn btn-default btn-sm" href="{{ route('student',['student_no'=>$score->student->no]) }}">查看學生資料</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/','HomeController@index');
+Route::group(['namespace'=>'Cool'], function () {
+    Route::get('cool','TestController@index');
 });
+
+//Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix'=>'student'], function () {
+    Route::get('{student_no}',['as'=>'student','uses'=>'StudentController@getStudentData'
+    ]);
+
+    Route::get('{student_no}/score/{subject?}',['as'=>'student.score','uses'=>'StudentController@getStudentScore'
+    ])->where(['subject'=>'(chinese|english|math)']);
+});
+Route::get('/board', 'BoardController@getIndex');
