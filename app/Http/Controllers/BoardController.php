b269467946a73b7785138aa413f89a794799c8c4
3A532027 <s3a532027@student.nuct.edu.tw>
2018-12-04 23:34:07 +0800

    練習4

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Score;
+use Route;
+use View;
+
+
+class BoardController extends Controller
+{
+    public function getIndex()
+    {
+        $scores=Score::orderByTotal()
+            ->orderBySubject()->get();
+        $data=['scores'=>$scores];
+        return view('board',$data);
+    }
+    public function getName()
+    {
+        return Route::currentRouteAction();
+    }
+
+}
